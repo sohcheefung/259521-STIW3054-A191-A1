4afff183079ffad9cd41a19ca4b9a39078123e1e
sohcheefung <gmail@sohcheefung>
2019-10-05 23:38:25 +0800

    version 1.2

--- src/main/java/com/mycompany/mavenproject4/link.java
+++ src/main/java/com/mycompany/mavenproject4/link.java
@@ -3,7 +3,7 @@ package com.mycompany.mavenproject4;
 /**
  *
  * @author Chee Fung
- */
+ 
 
 import java.io.IOException;
 import org.jsoup.Jsoup;
@@ -28,5 +28,48 @@ public class link {
       }    
       
   }
-}
+}*/
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.jsoup.Jsoup;
+import org.jsoup.helper.Validate;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+
+public class link {
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("https://github.com/STIW3054-A191/Main-Issues/issues/1").get();
+        String title = doc.title();
+        System.out.println("title :"+title);
+        System.out.println("----------------------------------------------------------------");
+        
+      
+            Elements data = doc.getElementsByClass("js-timeline-item js-timeline-progressive-focus-container");
+            //Elements data = doc.getElementsByTag("p");
+            for (Element content : data){
+                
+                String contentName = content.getElementsByTag("p").text();
+              
+             
+                System.out.println(contentName);
+                }
+
+            }
+            
+        }
+    
+
+
+  
+
+       
+    
+
 
