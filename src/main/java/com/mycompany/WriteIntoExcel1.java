cc7aae757517d6143b316ef6c1fa1e69b906621a
sohcheefung <gmail@sohcheefung>
2019-10-21 14:36:16 +0800

    version 2.1

--- src/main/java/com/mycompany/WriteIntoExcel1.java
+++ src/main/java/com/mycompany/WriteIntoExcel1.java
@@ -8,9 +8,14 @@ package com.mycompany;
 import org.apache.poi.hssf.usermodel.HSSFFont;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
 import java.io.File;
 import java.io.FileOutputStream;
+
 
 public class WriteIntoExcel1 {
     public void writeData() {
--- src/main/java/com/mycompany/WriteIntoExcel2.java
+++ src/main/java/com/mycompany/WriteIntoExcel2.java
@@ -9,7 +9,11 @@ package com.mycompany;
 import org.apache.poi.hssf.usermodel.HSSFFont;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
 import java.io.File;
 import java.io.FileOutputStream;
 
