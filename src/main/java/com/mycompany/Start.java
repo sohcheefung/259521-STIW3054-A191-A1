9a5ad7330ae283afbbff7af47ed320648b511175
sohcheefung <sohcheefung@gmail.com>
2020-02-07 19:07:13 +0800

    version 2.3

--- src/main/java/com/mycompany/Start.java
+++ src/main/java/com/mycompany/Start.java
@@ -5,6 +5,7 @@ package com.mycompany;
  * @author Chee Fung
  */
 public class Start {
+
     public static void main(String[] args) {
         try {
             WriteIntoExcel1 save = new WriteIntoExcel1();
@@ -12,20 +13,19 @@ public class Start {
             System.out.println("\nWriting data to Excel...");
             System.out.println("Excel has been saved");
             System.out.println("Done");
-            
+
             WriteIntoExcel2 save1 = new WriteIntoExcel2();
             save1.writeData();
             System.out.println("\nWriting data to Excel...");
             System.out.println("Excel has been saved");
             System.out.println("Done");
-            
+
             CompareData output = new CompareData();
             output.comparison();
-           
+
             System.out.println("END");
-           
-            
-        }catch(Exception e){
+
+        } catch (Exception e) {
             System.out.println("Program execution failed...");
         }
     }
